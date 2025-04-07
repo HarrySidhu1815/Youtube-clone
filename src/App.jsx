04927ef1247ec375a07157a055fc276422c11216
Harjobanpreet Sidhu <harjobanpreetsingh.sidhu@student.ufv.ca>
2025-04-07 13:45:59 -0700

    Video Page added and Frontend end for the website

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import Video from './Pages/Video/Video'
 const App = () => {
 
   const [sidebar, setSidebar] = useState(true)
-  
+
   return (
     <div>
       <Navbar setSidebar={setSidebar}/>
       <Routes>
         <Route path='/' element={<Home sidebar={sidebar}/>} />
-        <Route path='/videos/:categoryId/:videoId' element={<Video />} />
+        <Route path='/video/:categoryId/:videoId' element={<Video />} />
       </Routes>
     </div>
   )
